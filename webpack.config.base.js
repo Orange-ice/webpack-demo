1027cf604814029a7b8bb4d795c32c32504c3214
little_ice <2902978956@qq.com>
2020-07-05 22:04:19 +0800

    webpack上

---
+++ webpack.config.base.js
@@ -0,0 +1,15 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        filename: '[name].[contenthash].js'
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: "Webpack App",
+            template: "src/assets/index.html"
+        }),
+    ],
+};
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,23 @@
-var path = require('path');
+// 开发
+
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+const base = require('./webpack.config.base.js')
 
 module.exports = {
-    mode: 'development',
-    entry: './src/index.js',
-    output: {
-        filename: '[name].[contenthash].js'
-    }
+    ...base,
+    devtool: 'inline-source-map',
+    devServer: {
+        contentBase: './dist',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+        ],
+    },
 };
